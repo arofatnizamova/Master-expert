1e12b010b43d8955fca318ce0754447e7cb884ac
Your Name <you@example.com>
2025-03-03 13:14:39 +0500

    feat: add responsive servises section

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $('.slick-slider').each(function () {
+    let slider = $(this);
+    let options = {
+      prevArrow: slider.parent().find('.slider-prev'),
+      nextArrow: slider.parent().find('.slider-next'),
+      infinite: true,
+    }
+    let extraOptions = {}
+    if (slider.hasClass('services')) {
+      extraOptions = {
+        slidesToShow: 2,
+          slidesToScroll: 1,
+          centerMode: false,
+          arrows: true,
+          autoPlay: true,
+          dots: false,
+        responsive: [
+            {
+                breakpoint: 990,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  arrows: true,
+                }
+            }
+          
+        ] 
+      }
+    } 
+    slider.slick($.extend({}, extraOptions, options ));
+  })
+})
